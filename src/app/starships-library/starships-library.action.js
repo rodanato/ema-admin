5a42e172fb29efc86bde43393c0db974384f97c8
rod <rod.a.nato@gmail.com>
2018-03-23 17:25:00 -0500

    folder deleted

--- src/app/starships-library/starships-library.action.js
+++
@@ -1,9 +0,0 @@
-const saveStarshipsData = (data) => {
-  return {
-    type: 'SAVE_STARSHIPS_DATA',
-    data
-  }
-};
-
-export default saveStarshipsData;
-
--- src/app/starships-library/starships-library.container.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux'
-import StarshipsLibrary from './starships-library.component'
-
-const StarshipsLibraryContainer = connect(dispatch => dispatch)(StarshipsLibrary);
-
-export default StarshipsLibraryContainer;
