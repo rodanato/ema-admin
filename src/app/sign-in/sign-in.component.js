947a510ba02f6576997c39b023624515f2ab4298
Rodrigo Aguilar <rodrigo.aguilar@globant.com>
2018-04-04 15:58:20 -0500

    style(login): Login

--- src/app/sign-in/sign-in.component.js
+++ src/app/sign-in/sign-in.component.js
@@ -7,7 +7,18 @@ import { withRouter } from 'react-router-dom';
 import { auth } from 'config/firebase';
 import { Logger } from 'shared/utils';
 import withAuthorization from 'shared/components/with-authorization/with-authorization.component';
-import './sign-in.component.css'
+import './sign-in.component.css';
+
+const passwordValidation = password => !password || password.trim() === '' ? 'Ingrese una contraseña válida' : null;
+const userValidation = user => {
+  if (!user || user.trim() === '') {
+    return 'Ingrese una contraseña válida';
+  } else if (!user.match(/^\s*?(.+)@(.+?)\s*$/)) {
+    return 'Ingrese una contraseña válida';
+  } else {
+    return null;
+  }
+};
 
 class SignIn extends Component {
   state = {
@@ -17,71 +28,71 @@ class SignIn extends Component {
   };
 
   onSubmit(submittedValues) {
-    this.setState({ submittedValues });
-    console.log(submittedValues);
+    const {
+      history
+    } = this.props;
 
-    // e.preventDefault();
-    //
-    // const {
-    //   history
-    // } = this.props;
-    //
-    // auth.doSignInWithEmailAndPassword(this.state.email, this.state.password)
-    //   .then(() => {
-    //     Logger.log(`Logged In`);
-    //     history.push(routes.HOME);
-    //   })
-    //   .catch(error => {
-    //     Logger.error(`${error}`);
-    //   });
+    if (submittedValues.user &&
+        submittedValues.password) {
+      auth.doSignInWithEmailAndPassword(submittedValues.user, submittedValues.password)
+        .then(() => {
+          Logger.log(`Logged In`);
+          history.push(routes.HOME);
+        })
+        .catch(error => {
+          Logger.error(`${error}`);
+        });
+    }
   }
 
-
-
-  handleInputChange(e) {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  showError(errors, field) {
+    return (errors && errors[field]) ? errors[field] : null;
   }
 
   render() {
     return (
-      <div className={'columns is-centered'}>
-        <Form onSubmit={submittedValues => onSubmit(submittedValues)}>
-          {formApi => (
-            <form onSubmit={formApi.submitForm}
-                  className={'column is-half box login__container'}>
-              <div className="columns is-multiline">
-                <div className="column is-12">
-                  <label htmlFor="user">Usuario</label>
-                  <Text field="user"
-                        id="user" />
-                  {/*<input*/}
-                    {/*className={'input'}*/}
-                    {/*value={this.state.email}*/}
-                    {/*onChange={e => this.handleInputChange(e)}*/}
-                    {/*type="text"*/}
-                    {/*name="email"*/}
-                    {/*placeholder="Email"*/}
-                  {/*/>*/}
-                </div>
+      <section className="section login__section">
+        <img src={ process.env.PUBLIC_URL + '/images/logo.svg'}
+             className={'login__logo'}
+             alt='Logo de ema'/>
+        <div className={'columns is-centered'}>
+          <Form onSubmit={submittedValues => this.onSubmit(submittedValues)}
+                className={'login__form'}>
+            {formApi => (
+              <form onSubmit={formApi.submitForm}
+                    className={'column is-6'}>
+                <div className="columns is-multiline">
+                  <div className="column is-12 login__form-block">
+                    <Text field="user"
+                          validate={userValidation}
+                          className={'input login__input'}
+                          placeholder={'Usuario'}
+                          id="user" />
+                    <span className={'login__error'}>{ this.showError(formApi.errors, 'user') }</span>
+                  </div>
 
-                <div className="column is-12">
-                  <input
-                    className={'input'}
-                    value={this.state.password}
-                    onChange={e => this.handleInputChange(e)}
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                  />
-                  <button type="submit">
-                    Sign In
-                  </button>
+                  <div className="column is-12 login__form-block">
+                    <Text field="password"
+                          validate={passwordValidation}
+                          className={'input login__input'}
+                          placeholder={'Contraseña'}
+                          type={'password'}
+                          id="Password" />
+                    <span className={'login__error'}>{ this.showError(formApi.errors, 'password') }</span>
+                  </div>
+
+                  <div className="column is-12">
+                    <button className={'button ema-btn--success is-fullwidth login__submit'}
+                            type="submit">
+                      Login
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
-          )}
-        </Form>
-      </div>
+              </form>
+            )}
+          </Form>
+        </div>
+      </section>
     );
   }
 }
