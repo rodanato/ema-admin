01b242f5deeaf258c109082b9e382d4e81621102
rod <rod.a.nato@gmail.com>
2018-04-06 17:04:34 -0500

    core(firebase): Env config

--- src/config/firebase/firebase.js
+++ src/config/firebase/firebase.js
@@ -15,19 +15,34 @@ const configEnv = {
   qa: {
     API: 'https://ema-admin-console-qa.appspot.com/_ah/api/nina/',
     firebase: {
-
+      apiKey: "AIzaSyDlH_nfRlXnBV1Xk7FTME6B25NRsUV8hF0",
+      authDomain: "ema-admin-console-qa.firebaseapp.com",
+      databaseURL: "https://ema-admin-console-qa.firebaseio.com",
+      projectId: "ema-admin-console-qa",
+      storageBucket: "ema-admin-console-qa.appspot.com",
+      messagingSenderId: "1039914364880"
     }
   },
   uat: {
     API: 'https://ema-admin-console-uat.appspot.com/_ah/api/nina/',
     firebase: {
-
+      apiKey: "AIzaSyC0PKJymr67LpQZ3CCetjvq3Om6SnU1I0o",
+      authDomain: "ema-admin-console-uat.firebaseapp.com",
+      databaseURL: "https://ema-admin-console-uat.firebaseio.com",
+      projectId: "ema-admin-console-uat",
+      storageBucket: "",
+      messagingSenderId: "174391945125"
     }
   },
   prod: {
     API: 'https://ema-admin-console.appspot.com/_ah/api/nina/',
     firebase: {
-
+      apiKey: "AIzaSyDVeA2NCo0fiOnesrcUBZJHJOTeYxmPFLk",
+      authDomain: "ema-admin.firebaseapp.com",
+      databaseURL: "https://ema-admin.firebaseio.com",
+      projectId: "ema-admin",
+      storageBucket: "ema-admin.appspot.com",
+      messagingSenderId: "870901100380"
     }
   }
 };
